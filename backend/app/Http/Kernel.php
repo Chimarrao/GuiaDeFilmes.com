ca46b649e7179c9dd5fc24b6dc588034dc1209f3
Lucas Reolon <lucas1998lc@gmail.com>
2025-11-12 10:16:21 -0300

    Adicionado midleware no kernel

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -62,5 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'justwatch.restrict' => \App\Http\Middleware\RestrictJustWatchAccess::class,
     ];
 }
